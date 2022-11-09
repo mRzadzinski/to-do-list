0f04684a25ffe2bf20794b70424ccf1458b1d25b
Maciej Rzadzinski <maciej.rzd@gmail.com>
2022-11-09 20:35:57 +0100

    Style basic look of main task section

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './style.scss';
+import circleIcon from './img/circle-icon.png';
+
+let circleElements = document.querySelectorAll('.circle-icon');
+circleElements.forEach(element =>  { element.src = circleIcon });
+
