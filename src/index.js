f0d23cd66c8633ec6e673fbbad72c55105086d41
Maciej Rzadzinski <maciej.rzd@gmail.com>
2022-11-10 19:30:21 +0100

    Style finished section, partly style selected task

--- src/index.js
+++ src/index.js
@@ -4,3 +4,10 @@ import circleIcon from './img/circle-icon.png';
 let circleElements = document.querySelectorAll('.circle-icon');
 circleElements.forEach(element =>  { element.src = circleIcon });
 
+const picker = document.querySelector('.time-picker');
+
+const taskDateDivs = document.querySelectorAll('.task-date');
+console.log(taskDateDivs[0])
+
+taskDateDivs.forEach(div => 
+    div.addEventListener('click', () => picker.showPicker())); 
