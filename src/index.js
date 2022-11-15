f76c71543e4b97c9a240c6afa7a7d435483fc23c
Maciej Rzadzinski <maciej.rzd@gmail.com>
2022-11-15 17:56:21 +0100

    Implement drag & drop positioning feature

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import circleIcon from './img/circle-icon.png';
 import parseISO from 'date-fns/parseISO';
 
 // DOM manipulation section
+
+const allTasks = document.querySelectorAll('.task');
+const timePickers = document.querySelectorAll('.time-picker');
+const taskDateDivs = document.querySelectorAll('.task-date');
+const dateValues = document.querySelectorAll('.date-value');
 let circleElements = document.querySelectorAll('.circle-icon');
 circleElements.forEach(element =>  { element.src = circleIcon });
 
 // Date picker
-const timePickers = document.querySelectorAll('.time-picker');
-const taskDateDivs = document.querySelectorAll('.task-date');
-const dateValues = document.querySelectorAll('.date-value');
 let currentPicker;
 let currentDateValue;
+
 taskDateDivs.forEach(div => {
     div.addEventListener('click', () => {
         timePickers.forEach(picker => {
@@ -26,7 +29,6 @@ taskDateDivs.forEach(div => {
     div.addEventListener('change', toggleDate);
 });
     
-
 function convertDate() {
     let dateTimeArray = parseISO(currentPicker.value).toString().split(' ');
     const time = new Date(currentPicker.value).toLocaleTimeString('en', {
@@ -56,10 +58,15 @@ function toggleDate() {
 // Textarea auto resize and display value
 const textareas = document.getElementsByTagName('textarea');
 for (let i = 0; i < textareas.length; i++) {
-    textareas[i].setAttribute('style', 'height:' + (textareas[i].scrollHeight) + 'px;overflow-y:hidden;');
+    let height;
+    if (textareas[i].scrollHeight == 0) {
+        height = 'auto';
+    } else {
+        height = textareas[i].scrollHeight;
+    }
+    textareas[i].setAttribute('style', 'height:' + height + 'px;overflow-y:hidden;');
     textareas[i].addEventListener('input', updateTextareas, false);
     textareas[i].addEventListener('input', () => {
-        console.log(textareas[i].value)
         textareas[i].innerHTML =  textareas[i].value;
         textareas[i].previousElementSibling.innerHTML = textareas[i].value;
     });
@@ -70,9 +77,6 @@ function updateTextareas() {
 }
 
 // Expand clicked task
-
-const allTasks = document.querySelectorAll('.task');
-
 document.addEventListener('click', (e) => {
     allTasks.forEach(task => {
         if (task.contains(e.target)) {
@@ -81,5 +85,65 @@ document.addEventListener('click', (e) => {
             task.classList.remove('task-clicked');
         }
         toggleDate();
+    });
+});
+
+// Drag & drop
+let dragoverTarget;
+let dragSrcEl;
+
+document.addEventListener('DOMContentLoaded', (e) => {
+
+    function handleDragStart(e) {
+        this.style.opacity = '.5'
+        dragSrcEl = this;
+    }
+
+    function handleDragEnd(e) {
+        let lastTask = this.parentNode.lastChild;
+        this.style.opacity = '1';
+
+        // Move task to an end when dropping item outside of droppable area
+        if (dragSrcEl !== lastTask && lastTask.classList.contains('dragover-bottom')) {
+            lastTask.after(dragSrcEl);
+        }
+
+        allTasks.forEach(task => task.classList.remove('dragover-top', 'dragover-bottom'));
+    }
+
+    function handleDragOver(e) {
+        e.preventDefault();
+        dragoverTarget = this;
+
+        // Add / remove borders indicating drop position
+        if (e.target.classList.contains('task') && this !== dragSrcEl) {
+            this.classList.add('dragover-top');
+            dragSrcEl.parentNode.lastChild.classList.remove('dragover-bottom');
+        }
+        return false;
+    }
+
+    function handleDragLeave(e) {
+        
+        // Add / remove borders indicating drop position
+        this.classList.remove('dragover-top');
+        dragSrcEl.parentNode.lastChild.classList.add('dragover-bottom');
+    }
+
+    function handleDrop(e) {
+        e.stopPropagation();
+
+        if (dragSrcEl !== this && !this.parentNode.lastChild.classList.contains('dragover-bottom')) {
+            this.before(dragSrcEl);
+        }
+        return false;
+    }
+
+    allTasks.forEach(task => {
+        task.addEventListener('dragstart', handleDragStart);
+        task.addEventListener('dragover', handleDragOver);
+        task.addEventListener('dragleave', handleDragLeave);
+        task.addEventListener('dragend', handleDragEnd);
+        task.addEventListener('drop', handleDrop);
     });
 });
